de33192c108af61b8bda11722b5e69e776a1e839
Jan Paw <jan.paw@tesco.com>
2019-05-22 21:54:01 +0200

    - coordinator type aliases stub

--- src/main/scala/org/scoozie/dsl/Coordinator.scala
+++ src/main/scala/org/scoozie/dsl/Coordinator.scala
@@ -1,5 +1,27 @@
 package org.scoozie.dsl
 
-import org.scoozie.oozie.COORDINATORu45APP
+import org.scoozie.oozie.{
+  COMBINE, CONFIGURATIONType, CONTROLSType, COORDINATORu45APP, DATAIN, DATAOUT, DATASETS, INPUTEVENTS, INPUTLOGICOption,
+  LOGICALANDOption, LOGICALDATAIN, LOGICALOR, OUTPUTEVENTS, PARAMETERSType, WORKFLOW
+}
 
 trait Coordinator extends COORDINATORu45APP
+
+object Coordinator {
+
+  type Parameters = PARAMETERSType //TODO
+  type Controls = CONTROLSType //TODO
+  type Datasets = DATASETS //TODO
+  type InputEvents = INPUTEVENTS //TODO
+  type InputLogic = INPUTLOGICOption //TODO
+  type AND = LOGICALANDOption //TODO
+  type OR = LOGICALOR //TODO
+  type Combine = COMBINE //TODO
+  type LogicalDataIn = LOGICALDATAIN //TODO
+  type DataIn = DATAIN //TODO
+  type OutputEvents = OUTPUTEVENTS //TODO
+  type DataOut = DATAOUT //TODO
+  type Workflow = WORKFLOW //TODO
+  type Configuration = CONFIGURATIONType //TODO
+
+}
